dc5cf1baca5a47716dbc2b956e7c363977996971
Brian Cisneros <briancis@buffalo.edu>
2024-02-02 16:54:33 -0500

    show issues based on device

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
-import "./globals.css";
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
-import "@radix-ui/themes/styles.css";
+import "./globals.css";
 import "./theme-config.css";
-import { Theme, ThemePanel } from "@radix-ui/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,12 +19,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <Theme appearance="light" accentColor="purple">
           <NavBar />
           <main>{children}</main>
-          <ThemePanel />
+          {/* <ThemePanel /> */}
         </Theme>
       </body>
     </html>
