6b70c551f445eca2216f1577faaf444e803d1398
Brian Cisneros <briancis@buffalo.edu>
2024-02-18 00:13:38 -0500

    Added toast notifications

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,37 +1,42 @@
 'use client';
 
-import {User} from '@prisma/client';
-import {Select} from '@radix-ui/themes'
+import {Issue, User} from '@prisma/client';
+import {Select, Skeleton} from '@radix-ui/themes'
 import axios from 'axios';
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 import {useQuery} from "@tanstack/react-query";
-import {Skeleton} from "@radix-ui/themes";
+import {toast, Toaster} from "react-hot-toast";
 
-const AssigneeSelect = () => {
+const AssigneeSelect = ({issue}: { issue: Issue }) => {
 
     const {data: users, error, isLoading} = useQuery<User[]>({
-        queryKey: ['users'],
-        queryFn: () => axios.get('api/users').then(res => res.data),
-        staleTime: 60 * 1000, // 60s
+        queryKey: ['users'], queryFn: () => axios.get('api/users').then(res => res.data), staleTime: 60 * 1000, // 60s
         retry: 3
     })
 
-    if (isLoading) return <Skeleton />
+    if (isLoading) return <Skeleton/>
     if (error) return null
 
-    return (
-        <Select.Root>
+    return (<>
+        <Select.Root
+            defaultValue={issue.assignedToUserId || ""}
+            onValueChange={(userId) => {
+                axios.patch('/api/issues/' + issue.id, {assignedToUserId: userId || null}).catch(() => {
+                toast.error('Changes could not be saved')
+                })
+            }
+            }>
             <Select.Trigger placeholder='Assign...'/>
             <Select.Content>
                 <Select.Group>
                     <Select.Label>Suggestions</Select.Label>
-                    {users?.map(user => (
-                        <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>)
-                    )}
+                    <Select.Item value={""}>Unassigned</Select.Item>
+                    {users?.map(user => (<Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>))}
                 </Select.Group>
             </Select.Content>
         </Select.Root>
-    )
+        <Toaster/>
+    </>)
 }
 
 export default AssigneeSelect
