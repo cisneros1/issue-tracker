c160ba234d3eeb97024f6ed09d5bfa932f1a028f
Brian Cisneros <briancis@buffalo.edu>
2024-02-17 20:53:51 -0500

    Secure the application

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,51 +1,97 @@
 "use client";
 
-import classnames from "classnames";
+import { Skeleton } from "@/app/components";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import React from "react";
 import { AiFillBug } from "react-icons/ai";
-import {useSession} from "next-auth/react";
-import {Box} from "@radix-ui/themes"
+import classnames from "classnames";
+import { useSession } from "next-auth/react";
+import {
+    Avatar,
+    Box,
+    Container,
+    DropdownMenu,
+    Flex,
+    Text,
+} from "@radix-ui/themes";
 
 const NavBar = () => {
-  // Dependent on browser api therefore we need client api ('use client')
-  const currentPath = usePathname();
-  const {status, data:session} = useSession()
+    return (
+        <nav className="border-b mb-5 px-5 py-3">
+            <Container>
+                <Flex justify="between">
+                    <Flex align="center" gap="3">
+                        <Link href="/">
+                            <AiFillBug />
+                        </Link>
+                        <NavLinks />
+                    </Flex>
+                    <AuthStatus />
+                </Flex>
+            </Container>
+        </nav>
+    );
+};
 
-  const links = [
-    { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues" },
-  ];
+const NavLinks = () => {
+    const currentPath = usePathname();
 
-  return (
-    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-      <Link href="/">
-        <AiFillBug />
-      </Link>
-      <ul className="flex space-x-6">
-        {links.map((link) =>
-            <li key={link.href}>
-                <Link
-                    className={classnames({
-                        "text-zinc-950": link.href === currentPath,
-                        "text-zinc-500": link.href !== currentPath,
-                        "hover:text-zinc-800 transition-colors": true,
-                    })}
-                    href={link.href}
-                >
-                    {link.label}
-                </Link>
-            </li>
-            
-        )}
-      </ul>
+    const links = [
+        { label: "Dashboard", href: "/" },
+        { label: "Issues", href: "/issues/list" },
+    ];
+
+    return (
+        <ul className="flex space-x-6">
+            {links.map((link) => (
+                <li key={link.href}>
+                    <Link
+                        className={classnames({
+                            "nav-link": true,
+                            "!text-zinc-900": link.href === currentPath,
+                        })}
+                        href={link.href}
+                    >
+                        {link.label}
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+const AuthStatus = () => {
+    const { status, data: session } = useSession();
+
+    if (status === "loading") return <Skeleton width={"3rem"} />;
+
+    if (status === "unauthenticated")
+        return <Link className="nav-link" href="/api/auth/signin">Login</Link>;
+
+    return (
         <Box>
-            {status === "authenticated" && <Link href="/api/auth/signout">Sign Out</Link>}
-            {status === "unauthenticated" && <Link href="/api/auth/signin">Login</Link>}
-
+            <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                    <Avatar
+                        src={session!.user!.image!}
+                        fallback="?"
+                        size="2"
+                        radius="full"
+                        className="cursor-pointer"
+                    />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                    <DropdownMenu.Label>
+                        <Text size="2">{session!.user!.email}</Text>
+                    </DropdownMenu.Label>
+                    <DropdownMenu.Item onClick={() => href}>
+                        <Link href="/api/auth/signout">Log out</Link>
+                    </DropdownMenu.Item>
+                </DropdownMenu.Content>
+            </DropdownMenu.Root>
         </Box>
-    </nav>
-  );
+    );
 };
 
 export default NavBar;
--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,10 +1,17 @@
 import { issueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+import {getServerSession} from "next-auth";
+import authOptions from "@/app/auth/authOptions";
 
 export async function PATCH(
     request: NextRequest,
     { params }: { params: { id: string }}) {
+    const session = await getServerSession(authOptions)
+
+    if (!session)
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+
     const body = await request.json();
     const validation = issueSchema.safeParse(body);
     if (!validation.success)
@@ -30,6 +37,11 @@ export async function PATCH(
 export async function DELETE(
     request: NextRequest,
     { params }: { params: { id: string }}) {
+    const session = await getServerSession(authOptions)
+
+    if (!session)
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+
     const issue = await prisma.issue.findUnique({
         where: { id: parseInt(params.id) }
     });
