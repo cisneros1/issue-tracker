ccf99e509d3c6d95e7ec264b06a8c3de74f9d56e
Brian Cisneros <briancis@buffalo.edu>
2024-02-18 16:51:06 -0500

    refactored issue table

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
+import Pagination from "./components/Pagination";
 
-export default function Home() {
-  return (
-   <div>Hello World</div>
-  )
+export default function Home({searchParams}: {searchParams: {page: string}}) {
+  return <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)} />;
 }
